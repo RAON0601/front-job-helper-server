be9d7fc8df258bbd156daddc0311d4f972e37b45
김정민 <68500898+RAON0601@users.noreply.github.com>
2023-02-13 16:11:30 +0900

    토큰 검증 미들웨어 추가

---
+++ src/middleware/jwt.middleware.js
@@ -0,0 +1,29 @@
+import { JwtService } from "../service/jwt.service.js";
+
+export const jwtParseMiddleware = (req, res, next) => {
+  const jwtService = new JwtService();
+  const token = req.cookies.accessToken;
+
+  if (!token) {
+    next();
+    return;
+  }
+
+  const decoded = jwtService.verify(token);
+
+  // 토큰 만료일이 하루밖에 안남으면 토큰을 재발급합니다
+  const dayAmount = 60 * 60 * 24;
+  const millisecond = 1000;
+
+  if (Date.now() / millisecond - decoded.iat > dayAmount) {
+    const { email } = decoded;
+    const refreshToken = jwtService.generateToken({ email });
+    res.cookie.set("access_token", refreshToken, {
+      maxAge: process.env.JWT_EXPIRE_NUMBER,
+      httpOnly: true,
+    });
+  }
+
+  req.user = decoded;
+  next();
+};
