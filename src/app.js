498a3feff1dce41c06d54b2a7c29f194799279f8
김정민 <68500898+RAON0601@users.noreply.github.com>
2023-02-13 16:09:23 +0900

    설정파일 분리

--- src/app.js
+++ src/app.js
@@ -4,17 +4,25 @@ import { UserRepository } from "./repository/user.repository.js";
 import { PasswordEncoder } from "./service/passwordEncoder.js";
 import { UserService } from "./service/user.service.js";
 import { errorMiddleware } from "./middleware/error.middleware.js";
+import { JwtService } from "./service/jwt.service.js";
+import cookieParser from "cookie-parser";
+import { jwtParseMiddleware } from "./middleware/jwt.middleware.js";
 
 export class App {
   constructor() {
     this.app = express();
-    this.port = 6000;
+    this.port = process.env.SERVER_PORT || 6000;
   }
 
   initializeController() {
     const passwordEncoder = new PasswordEncoder();
     const userRepository = new UserRepository();
-    const userService = new UserService(userRepository, passwordEncoder);
+    const jwtService = new JwtService();
+    const userService = new UserService(
+      userRepository,
+      passwordEncoder,
+      jwtService
+    );
     const userController = new UserController(userService);
     const router = Router();
 
@@ -24,16 +32,19 @@ export class App {
 
   initMiddleware() {
     this.app.use(json());
+    this.app.use(cookieParser());
+    this.app.use(jwtParseMiddleware);
   }
 
   initErrorMiddleware() {
     this.app.use(errorMiddleware);
   }
 
-  startServer() {
+  async startServer() {
     this.initMiddleware();
     this.initializeController();
     this.initErrorMiddleware();
+
     this.app.listen(this.port, () =>
       console.log(`서버시작 http://localhost:${this.port}`)
     );
--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,17 @@
+import dotenv from "dotenv";
 import mysql from "mysql2/promise";
+dotenv.config();
 
-export const ConnectionPool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "204068",
-  database: "front_job_helper",
+const dbConfig = {
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER || "abc",
+  password: process.env.DATABASE_PASSWORD || "abd",
+  database: process.env.DATABASE_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   maxIdle: 10,
   idleTimeout: 60000,
   queueLimit: 0,
-});
+};
+
+export const ConnectionPool = mysql.createPool(dbConfig);
